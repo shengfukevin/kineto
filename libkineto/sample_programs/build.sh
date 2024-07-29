08ecf15d873332b18f7ec592de6f54a7bf2e60d1
Sheng Fu <shengfu@meta.com>
2024-07-29 15:31:28 -0700

    hard code to cuda 12.2

--- libkineto/sample_programs/build.sh
+++ libkineto/sample_programs/build.sh
@@ -5,23 +5,23 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-nvcc -c kineto_playground.cu -o kplay_cu.o
+nvcc -c kineto_playground.cu -o kplay_cu.o -I/usr/local/cuda-12.2/include
 
 g++ \
   -g3 \
   -O0 \
   kineto_playground.cpp \
   -o main \
-  -I/usr/local/cuda/include \
+  -I/usr/local/cuda-12.2/include \
   -I../third_party/fmt/include \
   -I/home/shengfu/local/kineto/libkineto/include \
   -L/usr/local/lib \
   -L/usr/local/cuda/lib64 \
   -lpthread \
   -lcuda \
-  -lcudart \
   -fsanitize=thread \
   /home/shengfu/local/kineto/libkineto/build/libkineto.a \
-  /usr/local/cuda/extras/CUPTI/lib64/libcupti.so \
-  /usr/local/cuda/extras/CUPTI/lib64/libnvperf_host.so \
+  /usr/local/cuda-12.2/extras/CUPTI/lib64/libcupti.so \
+  /usr/local/cuda-12.2/extras/CUPTI/lib64/libnvperf_host.so \
+  /usr/local/cuda-12.2/lib64/libcudart.so \
   kplay_cu.o
